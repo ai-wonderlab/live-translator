e8825d2f4b71a602daaa71cfca5fa8b575d74cea
Claudia Moutsai <claoumoutsai@gmail.com>
2026-03-27 14:57:01 +0200

    fix: UIRequiresFullScreen + UIView bg color for iOS 26 full screen

--- EasyLiveTranslator/EasyLiveTranslator/EasyLiveTranslatorApp.swift
+++ EasyLiveTranslator/EasyLiveTranslator/EasyLiveTranslatorApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import UIKit
 
 @main
 struct EasyLiveTranslatorApp: App {
+    init() {
+        // Force opaque full-screen background
+        UIView.appearance(whenContainedInInstancesOf: [UIWindow.self]).backgroundColor = UIColor(red: 0.030, green: 0.038, blue: 0.065, alpha: 1)
+    }
+
     var body: some Scene {
         WindowGroup {
             HomeView()
